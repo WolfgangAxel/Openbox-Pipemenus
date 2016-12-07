1a7938ab5e97bc96643a7235ed43049288d4acdb
Keaton <linux.keaton@gmail.com>
2016-12-06 20:26:51 -0600

    Update SteamPipe.py
    Overhauled the menu.
    + Script now checks if Steam is running, changes the Menu item to "Open Steam" if it is running and adds "Quit Steam" below that. (Good for taskbar-less setups like mine)
    + Added "Chat" menu
    ++ Can launch directly to the "Friends" window
    ++ Can change status or launch Steam with status set as Online, Busy, or Offline
    + Games moved into dedicated menu titled "Games"
    
    Future plans:
    I'd like to make a supplimentary file (SteamFriendsPipe.py) that looks up your friends and creates a pipe. The groundwork for this is already in place in the above script. It will probably will organize your friends by online/offline, open a chat window if you click their name, and have the status options. I haven't really figured out the logistics of this quite yet, but I'm sure it will require your steam account name/number and password being written in plain text somewhere.

--- SteamPipe/SteamPipe.py
+++ SteamPipe/SteamPipe.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#  SteamPipe.py.py
+#  
+#  2016 Keaton Brown <linux.keaton@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
 
 # Default is ['~/.steam/steam/steamapps/']
 # Add another steamapps folder by adding a comma just before the ] and pasting the directory with quotes around it
@@ -8,17 +23,58 @@ STEAMAPPS = ['~/.steam/steam/steamapps/']
 
 ### Menu building begins
 
-from os import popen
+from os import popen,path
 from re import search
 
-# Add Steam button to the top of the list, followd by separator
-print("""<openbox_pipe_menu>"
-  <item label='Steam'>
+# Begin
+print "<openbox_pipe_menu>"
+
+# Check to see if Steam is running, label buttons accordingly
+running = popen("ps axf | grep -i steam | grep -v SteamPipe | grep -v grep").read()
+if not running:
+	print("""  <item label='Steam'>
+    <action name='Execute'>
+      <execute>steam</execute>
+    </action>
+  </item>""")
+else:
+	print("""  <item label='Open Steam'>
     <action name='Execute'>
       <execute>steam</execute>
     </action>
   </item>
-  <separator/>""")
+  <item label='Quit Steam'>
+    <action name='Execute'>
+      <execute>steam -shutdown</execute>
+    </action>
+  </item>""")
+if path.exists(path.dirname(path.realpath(__file__))+"/SteamFriendsPipe.py"):
+	print '  <menu execute="python '+path.dirname(path.realpath(__file__))+'"/SteamFriendsPipe.py" id="steamfriendspipe" label="Chat"/>'
+else:
+	print("""  <menu id="steamchat" label="Chat">
+    <item label="Open Friends">
+      <action name="execute">
+        <execute>steam steam://open/friends</execute>
+      </action>
+    </item>
+    <item id="statusOnline" label="Status: Online">
+      <action name="execute">
+        <execute>steam steam://friends/status/online</execute>
+      </action>
+    </item>
+    <item id="statusOnline" label="Status: Busy">
+      <action name="execute">
+        <execute>steam steam://friends/status/busy</execute>
+      </action>
+    </item>
+    <item id="statusOnline" label="Status: Offline">
+      <action name="execute">
+        <execute>steam steam://friends/status/offline</execute>
+      </action>
+    </item>
+  </menu>
+""")
+print '  <menu id="steamGames" label="Games">'
 GAMES=[]
 for DIR in STEAMAPPS:
 	# Make sure all DIR are in the same format
@@ -38,11 +94,12 @@ for DIR in STEAMAPPS:
 for GAME in sorted(GAMES):
 	NAME,ID=GAME
 	# Make the entry
-	print '  <item label="'+NAME.replace('&','&amp;')+'">'
-	print '    <action name="Execute">'
-	print "      <execute>steam steam://run/"+ID+"</execute>"
-	print "    </action>"
-	print "  </item>"
+	print '    <item label="'+NAME.replace('&','&amp;').replace("'","&apos;")+'">'
+	print '      <action name="Execute">'
+	print "        <execute>steam steam://run/"+ID+"</execute>"
+	print "      </action>"
+	print "    </item>"
 
 # End the menu
-print "</openbox_pipe_menu>"
+print("""  </menu>
+</openbox_pipe_menu>""")
