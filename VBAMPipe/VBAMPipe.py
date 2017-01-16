6b64861c3123dd61b87e65836e004e0bc5708ca0
Keaton <linux.keaton@gmail.com>
2017-01-15 20:20:49 -0600

    Update VBAMPipe.py
    + Updated to Python3
    + New argument -d
     + Used to specify GameBoy directory
     + Defaults to ~/Games/GameBoy
     + If -d fails or isn't used and the default doesn't exist, prints only a label asking for the -d argument and a valid directory
    + Added XMLFriendly function for improved readability

--- VBAMPipe/VBAMPipe.py
+++ VBAMPipe/VBAMPipe.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 #  VBAMPipe.py
@@ -22,27 +22,43 @@
 # If you don't have the gba_bios.bin, delete "--bios="+gamedir+"gba_bios.bin"
 # in the command below.
 
-# Paste the full path to your /GameBoy folder inbetween the single quotes
-gamedir=''
+from sys import argv
+from os.path import exists,expanduser
+
+args = [ arg for arg in argv ]
+
+gamedir = ''
+if "-d" in args:
+	gamedir = expanduser(args[args.index("-d")+1])
+if not exists(gamedir):
+	gamedir = expanduser("~/Games/GameBoy")
+	if not exists(gamedir):
+		print("<openbox_pipe_menu>\n"
+			  "  <separator label='Please use -d to specify an existing GameBoy directory'/>\n"
+			  "</openbox_pipe_menu>")
+		exit()
 
 ## Imports
 from os import popen
-import re
+from re import match
+
+def XMLFriendly(s):
+	return s.replace('&','&amp;').replace("'","&apos;").replace("_","__")
 
 ## Menu building
-print "<openbox_pipe_menu>"
+print("<openbox_pipe_menu>")
 # Because I'm both inconsistent and lazy
 if gamedir[-1] != '/':
 	gamedir=gamedir+'/'
 for game in popen("ls "+gamedir+"*.g*").readlines():
   # Get the name of the game from the ls output
-	name = re.match('(?i)'+gamedir+'(.*).g.*',game).group(1).replace('&','&amp;').replace("'","&apos;").replace("_","__")
-	print '  <item label="'+name+'">'
-	print '    <action name="Execute">'
+	name = XMLFriendly(match('(?i)'+gamedir+'(.*).g.*',game).group(1))
+	print('  <item label="'+name+'">')
+	print('    <action name="Execute">')
   # Kill pulseaudio, launch the game, then restart pulseaudio.
   # Pulse has always caused vbam to sporatically crash for me
-	print "      <execute>sh -c \"pulseaudio -k; vbam --opengl-nearest --pause-when-inactive --flash-size=1 --bios="+gamedir.replace('&','&amp;').replace("'","&apos;")+"gba_bios.bin --rtc '"+game[:-1].replace('&','&amp;').replace("'","&apos;")+"';pulseaudio --start\"</execute>"
-	print "    </action>"
-	print "  </item>"
+	print("      <execute>sh -c \"pulseaudio -k; vbam --opengl-nearest --pause-when-inactive --flash-size=1 --bios="+XMLFriendly(gamedir)+"gba_bios.bin --rtc '"+XMLFriendly(game[:-1])+"';pulseaudio --start\"</execute>")
+	print("    </action>")
+	print("  </item>")
 
-print "</openbox_pipe_menu>"
+print("</openbox_pipe_menu>")
